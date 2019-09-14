7a883c3f0384745189ab9966bfb6188399c74cc9
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2019-09-14 16:34:51 -0400

    #1 added basic schemas to our db

--- tutify/backend/data.js
+++ tutify/backend/data.js
@@ -2,14 +2,99 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// this will be our data base's data structure 
-const DataSchema = new Schema(
-  {
-    id: Number,
-    message: String
+
+const TutorSchema = new Schema({
+  id: {
+    type: Number,
+    required: true
   },
-  { timestamps: true }
-);
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  subject: {
+    type: String,
+    required: true
+  }
+});
+
+model.exports = mongoose.model('Tutor', TutorSchema);
+
+const UserSchema = new Schema({
+    id: {
+      type: Number,
+      required: true
+    },
+    first_name: {
+      type: String,
+      required: true
+    },
+    last_name: {
+      type: String,
+      required: true
+    },
+    education: {
+      type: String,
+      required: true
+    },
+    school: {
+      type: String,
+      required: true
+    }
+
+});
+
+model.exports = mongoose.model('User', UserSchema);
+
+const AccountSchema = new Schema({
+    id: {
+      type: Number,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+
+});
+
+model.exports = mongoose.model('Account', AccountSchema);
+
+const AppointmentSchema = new Schema({
+  tutor_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'Tutor',
+    required: true
+  },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  //Is this necessary? Since mongodb isn't relational db, i don't think this is needed
+  id: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  location: {
+    type: String
+  }
+});
+
+model.exports = mongoose.model('Appointment', AppointmentSchema);
+
 
 // export the new Schema so we could modify it using Node.js
 module.exports = mongoose.model("Data", DataSchema);
