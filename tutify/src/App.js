0a9380e55d5603924dab403b19d59363e1763b32
compgirl123 <claudia.f.feochari@hotmail.com>
2019-09-15 19:09:37 -0400

    #8 : Created Front-End page for Sign Up Page with fields requested by our stakeholders

--- tutify/src/App.js
+++ tutify/src/App.js
@@ -1,5 +1,18 @@
-// /client/App.js
 import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import large_tutify from './assets/large_tutify.png';
+import MenuItem from "@material-ui/core/MenuItem";
 import axios from 'axios';
 
 class App extends Component {
@@ -163,4 +176,313 @@ class App extends Component {
   }
 }
 
-export default App;
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'© '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Tutify
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const educationLevel = [
+  {
+    value: "elementary",
+    label: "Elementary School"
+  },
+  {
+    value: "highschool",
+    label: "High School"
+  },
+  {
+    value: "cegep",
+    label: "Cegep"
+  },
+  {
+    value: "university",
+    label: "University"
+  },
+  {
+    value: "adulteducation",
+    label: "Adult Education"
+  }
+];
+
+const classesTutor = [
+  {
+    value: "chem204",
+    label: "chem204"
+  },
+  {
+    value: "chem205",
+    label: "chem205"
+  },
+  {
+    value: "math204",
+    label: "math204"
+  },
+  {
+    value: "math205",
+    label: "math205"
+  }
+];
+
+const typeOFTutoring = [
+  {
+    value: "crashcourse",
+    label: "Crash Course"
+  },
+  {
+    value: "weeklytutoring",
+    label: "Weekly Tutoring"
+  },
+  {
+    value: "oneononetutoring",
+    label: "One on One Tutoring"
+  },
+  {
+    value: "grouptutoring",
+    label: "Group Tutoring"
+  }
+];
+
+ 
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: "#2fb62f",
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor:'white',
+    padding:'15px'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    name: "Cat in the Hat",
+    age: "",
+    multiline: "Controlled",
+    currency: "EUR"
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+         <img src={large_tutify} className="App-logo" alt="logo" />
+        <Typography component="h1" variant="h5">
+          Student Sign Up
+
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+          <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="Username"
+                label="Username"
+                name="Username"
+                autoComplete="username"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            
+            <Grid item xs={12}>
+                  <TextField
+              id="standard-select-educationLevel"
+              select
+              label="What Describes your Education Level?"
+              required
+              fullWidth
+              className={classes.textField}
+              value={values.educationLevel}
+              onChange={handleChange("educationLevel")}
+              SelectProps={{
+                MenuProps: {
+                  className: classes.menu
+                }
+              }}
+              helperText="Please select which level of education you would like tutoring for"
+              margin="normal"
+            >
+              {educationLevel.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+      </TextField>
+      </Grid>
+
+      <Grid item xs={12}>
+                  <TextField
+              id="standard-select-classesTutor"
+              select
+              label="What Classes do you want to be tutored in?"
+              required
+              fullWidth
+              className={classes.textField}
+              value={values.classesTutor}
+              onChange={handleChange("classesTutor")}
+              SelectProps={{
+                MenuProps: {
+                  className: classes.menu
+                }
+              }}
+              helperText="Please select which classes you would like tutoring for"
+              margin="normal"
+            >
+              {classesTutor.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+      </TextField>
+      </Grid>
+
+      <Grid item xs={12}>
+                  <TextField
+              id="standard-select-typeOFTutoring"
+              select
+              label="What Type of Tutoring are you looking for?"
+              required
+              fullWidth
+              className={classes.textField}
+              value={values.typeOFTutoring}
+              onChange={handleChange("typeOFTutoring")}
+              SelectProps={{
+                MenuProps: {
+                  className: classes.menu
+                }
+              }}
+              helperText="Please select which type tutoring service (crash course, weekly tutoring, one on one, group tutoring) 
+              you would like."
+              margin="normal"
+            >
+              {typeOFTutoring.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+      </TextField>
+      </Grid>
+
+
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I Agree that Tutify will keep all data provided private from third-parties and will only use the data provided to best match a student with a tutor."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
