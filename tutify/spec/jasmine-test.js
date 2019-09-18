87ba30bcef801c066ecf24548dc0422baa9c6216
compgirl123 <claudia.f.feochari@hotmail.com>
2019-09-18 15:27:51 -0400

    #2 : Added a jasmine test function from a folder outside of the test directory. This function is a simple function that evaluates if 2+2 equals 4. Functions concerning the Sign up page and Login page will be added in the future

--- tutify/spec/jasmine-test.js
+++ tutify/spec/jasmine-test.js
@@ -1,5 +1,26 @@
+var fileSystem = require('fs');
+
+// Importing a .js file in the src folder
+var contents = fileSystem.readFileSync('./src/signUpPageLogic.js', 'utf-8',function(err, data) {});
+var contentsModified = "var fn = " + contents;
+
+// taking the string content from contentsModified and converting it into a function
+eval(contentsModified);
+fn();
+console.log(fn());
+
+// simple test function that is locally contained within the jasmine-test.js file.
 describe("A suite", function() {
     it("contains spec with an expectation", function() {
       expect(true).toBe(true);
     });
   });
+
+// Simple test function that is contained outside of the directory and in the src/signUpPageLogic.js directory.
+// This function simply tests if the addition of 2+2 is equal to 4.
+// In the future, we will be adding functions that test the functionality of the Sign Up Page within this group of functions.
+describe("Tests for Sign Up Page", function(){
+	it("addition function", function(){
+		expect(fn()).toBe(true);
+	});
+});
