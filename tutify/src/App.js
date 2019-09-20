12d9fe11b43d8bc21f89a891e2c227a017f3c8b5
Pierre Watine <watinepierre@gmail.com>
2019-09-20 18:15:35 -0400

    #3 : Refactored routing

--- tutify/src/App.js
+++ tutify/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import Main from "./components/Main";
 import SignUp from "./components/SignUp"
 import Database from "./components/Database"
+import SearchResults from "./components/SearchResults"
 
 // This is all the url routing.
 function MainPage() {
@@ -11,6 +12,7 @@ function MainPage() {
         <Route exact path="/" component={Main} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/database" component={Database} />
+        <Route exact path="/SearchResults" component={SearchResults} />
     </Router>
   );
 }
