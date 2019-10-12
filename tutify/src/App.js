90086b4da6f1f7302d242e539cdcf1091dde4bc6
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2019-10-12 14:44:05 -0400

    #42 removed unused files

--- tutify/src/App.js
+++ tutify/src/App.js
@@ -1,12 +1,9 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Main from "./components/Main";
 import SignUp from "./components/SignUp"
 import Database from "./components/Database"
 import SearchResults from "./components/SearchResults"
-import Database2 from "./components/combination"
-import Tester from "./components/demo"
 import Login from "./components/Login"
 
 
@@ -18,13 +15,10 @@ function MainPage() {
         <Route exact path="/" component={Main} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/database" component={Database} />
-        <Route exact path="/signupwithdatabase" component={Database2} />
         <Route exact path="/search_results" component={SearchResults} />
-        <Route exact path="/demo" component={Tester} />
         <Route exact path="/login" component={Login} />
     </Router>
   );
-
 }
 
 // This is the component that will be shown by default
