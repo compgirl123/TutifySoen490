97364126a50dc24dc992b19876b0e372ca44a5ad
Cynthia <cynthia.cherrier@hotmail.com>
2019-10-12 20:50:42 -0400

    Fixed merge errors

--- tutify/src/styles/SignUp-styles.js
+++ tutify/src/styles/SignUp-styles.js
@@ -81,11 +81,5 @@ export const styles = theme => ({
   gridContainer: {
     background: theme.palette.background.paper,
   },
-  school: {
-    color: '#969696',
-  },
-  program: {
-    color: '#8ae600',
-  },
   
 });
