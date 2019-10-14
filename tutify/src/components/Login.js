baabb44a4c69ea4e5b7b39408f2e31b6a8cd9bba
compgirl123 <claudia.f.feochari@hotmail.com>
2019-10-14 19:36:12 -0400

    #56 : Fixed the problem with encryption that we were having. Website should now allow users to enter their passwords and these are stored as encrypted objects in the database and the user can log in with their own password, not the encryption

--- tutify/src/components/Login.js
+++ tutify/src/components/Login.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
-import Container from '@material-ui/core/Container'; 
+import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import large_tutify from './../assets/large_tutify.png';
 import * as tutifyStyle from '../styles/SignUp-styles';
@@ -13,135 +13,137 @@ import Footer from './Footer';
 import './style.css';
 
 class Login extends React.Component {
- // initialize our state
-   // initialize our state
-   constructor(props) {
+  // initialize our state
+  // initialize our state
+  constructor(props) {
     super(props);
     this.state = {
       data: [],
-      email : null,
+      email: null,
       password: null,
     };
-    this.handleSubmit = this.handleSubmit.bind(this);      
-    }
-    
-    // when component mounts, first thing it does is fetch all existing data in our db
-    // then we incorporate a polling logic so that we can easily see if our db has
-    // changed and implement those changes into our UI
-    componentDidMount() {
-      this.getDataFromDb();
-    
-    }
-    
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  
-    // fetch data from the data base
-    getDataFromDb = () => {
-      fetch('http://localhost:3001/api/getUser')
-        .then((data) => data.json())
-        .then((res) => this.setState({ data: res.data }));
-    };
-  
-//Authenticates User when submit button is pressed
-    handleSubmit(event){
-      event.preventDefault();
-      fetch('http://localhost:3001/api/authUser', {
-        method: 'POST',
-        credentials: 'include',
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify({
-        "email" : document.getElementById("email").value,
+  // when component mounts, first thing it does is fetch all existing data in our db
+  // then we incorporate a polling logic so that we can easily see if our db has
+  // changed and implement those changes into our UI
+  componentDidMount() {
+    this.getDataFromDb();
+
+  }
+
+
+
+  // fetch data from the data base
+  getDataFromDb = () => {
+    fetch('http://localhost:3001/api/getUser')
+      .then((data) => data.json())
+      .then((res) => this.setState({ data: res.data }));
+  };
+
+  //Authenticates User when submit button is pressed
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch('http://localhost:3001/api/authUser', {
+      method: 'POST',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        "email": document.getElementById("email").value,
         "password": document.getElementById("password").value
-         })
       })
+    })
       .then(response => response.json())
       .then(res => {
-        if(res.isLoggedIn){
+        if (res.isLoggedIn) {
           alert("Signed in");
           this.props.history.push("/profile");
         }
-        else{
+        else {
+          console.log(res);
+          alert(document.getElementById("password").value);
           alert("Invalid use password");
         }
       })
       .catch(err => console.log(err));
-    };
+  };
 
   render() {
     const { classes } = this.props;
-   
+
     return (
-    <div>
-            <NavBar />
+      <div>
+        <NavBar />
 
-    <Container component = "main">
-     <CssBaseline />
-      <div className = "paper">
-       <img src={large_tutify} className="App-logo" alt="logo" />
-      <form className="form" noValidate onSubmit={this.handleSubmit}
->
-      <Grid container spacing={3}
-  direction="column"
-  style={{ minHeight: '40vh'}}>
-           
-            <Grid item xs={6}>
-              <TextField
-                variant="outlined"
-                required
-                style = {{width: 350}}
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                InputProps={{
-                  classes: {
-                    notchedOutline: classes.notchedOutline
-                  }
-                }}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                variant="outlined"
-                required
-                style = {{width: 350}}
-                name="password"
-                label="password"
-                type="password"
-                id="password"
-                autoComplete="password"
-                InputProps={{
-                  classes: {
-                    notchedOutline: classes.notchedOutline
-                  }
-                }}
-              />
-            </Grid>
-          
-             <Grid item xs={6} >
-            <Button
-            type="submit"
-            style = {{width: 350}}
-            variant="contained"
-            className="submit"
-          >
-            Login
+        <Container component="main">
+          <CssBaseline />
+          <div className="paper">
+            <img src={large_tutify} className="App-logo" alt="logo" />
+            <form className="form" noValidate onSubmit={this.handleSubmit}
+            >
+              <Grid container spacing={3}
+                direction="column"
+                style={{ minHeight: '40vh' }}>
+
+                <Grid item xs={6}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    style={{ width: 350 }}
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    InputProps={{
+                      classes: {
+                        notchedOutline: classes.notchedOutline
+                      }
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    style={{ width: 350 }}
+                    name="password"
+                    label="password"
+                    type="password"
+                    id="password"
+                    autoComplete="password"
+                    InputProps={{
+                      classes: {
+                        notchedOutline: classes.notchedOutline
+                      }
+                    }}
+                  />
+                </Grid>
+
+                <Grid item xs={6} >
+                  <Button
+                    type="submit"
+                    style={{ width: 350 }}
+                    variant="contained"
+                    className="submit"
+                  >
+                    Login
           </Button>
-            <Grid item style={{paddingTop:'20px'}}>
-              <Link href="#" variant="body2" justify="flex-end">
-                Forgot password?
+                  <Grid item style={{ paddingTop: '20px' }}>
+                    <Link href="#" variant="body2" justify="flex-end">
+                      Forgot password?
               </Link>
-          </Grid>
-          </Grid>
-          </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
 
-       
-          
-      </form>
-    </div>
-   <Footer/>
-       </Container>
-       </div>
+
+
+            </form>
+          </div>
+          <Footer />
+        </Container>
+      </div>
     );
   }
 }
