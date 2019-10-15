432aa9d98e7fffe2af9343230d2f5000e44ac26f
tanyamultani <tanyamultani1@gmail.com>
2019-10-15 12:45:28 -0400

    tutify logo redirects to homepage if not logged in #59

--- tutify/src/components/profilePage/DashBoardNavBar.js
+++ tutify/src/components/profilePage/DashBoardNavBar.js
@@ -11,7 +11,7 @@ import clsx from 'clsx';
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { NavDrawer } from "./NavDrawer";
-import Button from '@material-ui/core/Button';
+import { Link } from '@material-ui/core';
 
 
 export class NavBar extends Component {
@@ -75,9 +75,13 @@ export class NavBar extends Component {
           
           <SchoolIcon />
           <Box m={1} /> 
+          {this.state.Toggle ? <Typography variant="h6" color="inherit" >
+              Tutify 
+            </Typography>:<Link href="/" style={{textDecoration: 'none', color: '#FFF'}}>
             <Typography variant="h6" color="inherit" >
               Tutify 
             </Typography>
+          </Link>}
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
           </Typography>
        
