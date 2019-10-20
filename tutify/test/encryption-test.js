1f0c85fc85b1c12fac23248797eb40a8f3df07c6
Pierre Watine <watinepierre@gmail.com>
2019-10-19 23:18:54 -0400

    #58: Install Mocha and Chai

---
+++ tutify/test/encryption-test.js
@@ -0,0 +1,12 @@
+const testFunctions = require("./../backend/controllers/user.controller");
+const assert = require("chai").assert;
+describe("Encryption tests: ", function(){
+    var text = "B3$A1o$%a!";
+
+    it("encrypt() and decrypt()", function(){
+        var eText= testFunctions.encrypt(text);
+        assert(eText !== text);
+
+        assert(testFunctions.decrypt(eText)===text);
+    });
+});
