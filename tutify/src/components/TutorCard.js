17378ae0e5035939f5e04580280a9210c69463e1
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2019-10-25 15:18:00 -0400

    #34 added a Dialog window that will display tutor profile information

--- tutify/src/components/TutorCard.js
+++ tutify/src/components/TutorCard.js
@@ -7,13 +7,27 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip'
 import CardActionArea from '@material-ui/core/CardActionArea';
-import { BrowserRouter as Link } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 import * as tutifyStyle from '../styles/SearchTutors-styles';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { ButtonBase } from '@material-ui/core';
 
 class TutorCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+            scroll: 'paper'
+        };
+        this.handleClickOpen = this.handleClickOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
 
     assignTutor(e, tutor) {
         axios.post('http://localhost:3001/api/assignTutor', {
@@ -22,20 +36,43 @@ class TutorCard extends Component {
         });
     }
 
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
     render() {
         const { classes } = this.props
         const { tutor } = this.props
-
+        const { open } = this.state
+        const { scroll } = this.state
         return (
             <Grid item xs={12} sm={6} md={4}>
+                <Dialog
+                    open={open}
+                    scroll={scroll}
+                    aria-labelledby="scroll-dialog-title"
+                >
+                    <DialogActions>
+                        <Button onClick={this.handleClose}>Close</Button>
+                    </DialogActions>
+                    <DialogTitle id="scroll-dialog-title"></DialogTitle>
+                    <DialogContent>
+                    </DialogContent>
+                </Dialog>
                 <Card className={classes.card}>
-                    <CardActionArea component={Link} to="/profile_tutor">
-                        <CardMedia
-                            className={classes.cardMedia}
-                            image={tutor.picture}
-                            title={tutor.first_name + " " + tutor.last_name}
-                        />
-                    </CardActionArea>
+                    <ButtonBase onClick={this.handleClickOpen}>
+                        <CardActionArea>
+                            <CardMedia
+                                className={classes.cardMedia}
+                                image={tutor.picture}
+                                title={tutor.first_name + " " + tutor.last_name}
+                            />
+                        </CardActionArea>
+                    </ButtonBase>
                     <CardContent className={classes.cardContent}>
                         <Typography variant="h5" component="h2">
 
