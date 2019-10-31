a3a52a306c965dd806af39dbc8badeffa59187c2
tanyamultani <tanyamultani1@gmail.com>
2019-10-31 14:42:45 -0400

    refactored sidebar to make it easier to modify #79

--- tutify/src/components/profilePage/StudentSidebar.js
+++ tutify/src/components/profilePage/StudentSidebar.js
@@ -14,35 +14,17 @@ import {List} from '@material-ui/core';
 import Divider from '@material-ui/core/Divider';
 import PublishIcon from '@material-ui/icons/Publish';
 import SchoolIcon from '@material-ui/icons/School';
+
+
 export class Sidebar extends Component {
  
  
   constructor(props) {
     super(props);
-    this.state = {
-      Toggle: false
-    };
+    
     this.handleChange = this.handleChange.bind(this);
     }
-    componentDidMount() {
-      this.checkSession();
-    }
-  checkSession = () => {
-      fetch('http://localhost:3001/api/checkSession',{
-                    method: 'GET',
-                    credentials: 'include'
-      })          
-        .then(response => response.json())
-        .then(res => {
-          if(res.userInfo.__t === 'student'){
-              this.setState({Toggle: true});
-          }
-          else if(res.userInfo.__t === 'tutor'){
-              this.setState({Toggle: false});
-          }
-        })
-        .catch(err => console.log(err));
-      };
+    
     handleChange(event){
       fetch('http://localhost:3001/api/logout',{
                     method: 'GET',
@@ -57,7 +39,6 @@ export class Sidebar extends Component {
       return(
         <div>
           <Divider />
-    { this.state.Toggle ? 
 <List>  
   <div>
     <ListItem button component="a" href="/profile">
@@ -94,41 +75,10 @@ export class Sidebar extends Component {
 </ListItem>
 </div>
 </List>
-    :
-<List>  
-  <div>
-    <ListItem button component="a" href="/profile">
-      <ListItemIcon>
-        <AccountBoxIcon />
-      </ListItemIcon>
-      <ListItemText primary="My Profile" />
-    </ListItem>
-     <ListItem button component="a" href="/upload">
-      <ListItemIcon>
-        <PublishIcon/>
-      </ListItemIcon>
-      <ListItemText primary="Upload Documents" />
-    </ListItem>
-    <ListItem button component="a" href="/students">
-      <ListItemIcon>
-        <SchoolIcon/>
-      </ListItemIcon>
-      <ListItemText primary="My Students" />
-    </ListItem>
-  
-  <ListItem button component="a" href="/" onClick={this.handleChange} >
-      <ListItemIcon>
-        <ExitToAppIcon />
-      </ListItemIcon>
-      <ListItemText primary="Logout" />
-    </ListItem>
-  </div>
-  </List>
-  }
+   
    
   
   <Divider/>
-  { this.state.Toggle ? 
   
   <List>
   <div>
@@ -153,7 +103,7 @@ export class Sidebar extends Component {
     </ListItem>
   </div>
   </List>
-    : null}
+    
   </div>
       );
     }
