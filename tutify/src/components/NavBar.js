cc96e82e9762181eaa81b82e36cff380fe74216f
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2019-11-01 15:52:59 -0400

    #72 made navdrawer permanent component in UserDashboard

--- tutify/src/components/NavBar.js
+++ tutify/src/components/NavBar.js
@@ -82,15 +82,18 @@ export class NavBar extends Component {
     return (
       
       <div className={classes.root}>
-        <AppBar position="fixed" color="inherit" elevation={0}  style = {{background: 'linear-gradient(45deg, rgba(0,200,83,1) 0%, rgba(200,255,75,1) 100%)'}}>
-          <Toolbar style={{color: 'white'}}>
-            <SchoolIcon className={classes.icon}/>
-            <Box m={1} /> 
-          
-            <Link href="/" className={classes.title} style={{textDecoration: 'none', color: '#FFF'}}>
-              <Typography variant="h6" color="inherit" >
-                Tutify
-              </Typography>
+        <AppBar className={classes.appBar} position="fixed" color="inherit" elevation={0}  style = {{background: 'linear-gradient(45deg, rgba(0,200,83,1) 0%, rgba(200,255,75,1) 100%)'}}
+        >
+        <Toolbar style={{
+          color: 'white'}} >
+
+          <SchoolIcon className={classes.icon}/>
+          <Box m={1} /> 
+         
+          <Link href="/" className={classes.title} style={{textDecoration: 'none', color: '#FFF'}}>
+            <Typography variant="h6" color="inherit" >
+              Tutify
+            </Typography>
             </Link>
               
             <div className={classes.buttonContain}>
