59a8e5421d37795bc50475ed499cc4429f91b5f0
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2019-11-01 16:08:30 -0400

    #72 fixed container where dashboard content is displayed

--- tutify/src/styles/UserDashboard/UserDashboard-styles.js
+++ tutify/src/styles/UserDashboard/UserDashboard-styles.js
@@ -4,6 +4,11 @@ export const styles = theme => ({
         flexGrow: 1,
         paddingTop: theme.spacing(10),
         paddingBottom: theme.spacing(8),
+        marginLeft:'200px'
+      },
+      drawer: {
+          width: 240,
+          flexShrink: 0,
       },
       toolbar: theme.mixins.toolbar,
   });
