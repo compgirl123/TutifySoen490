8774e1a60bb80221789cebb25e94b22010e1909c
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2019-11-03 12:04:00 -0500

    #72 made table header sticky for course list and transformed to class component

--- tutify/src/components/UserDashboardPage/MyCourseList.js
+++ tutify/src/components/UserDashboardPage/MyCourseList.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import * as UserDashboardStyles from '../../styles/UserDashboard/UserDashboard-styles';
+import { withStyles } from "@material-ui/core/styles";
 
 function createData(name, calories, fat, carbs, protein) {
     return { name, calories, fat, carbs, protein };
@@ -19,41 +18,48 @@ const rows = [
     createData('Eclair', 262, 16.0, 24, 6.0),
     createData('Cupcake', 305, 3.7, 67, 4.3),
     createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
-export default function MyCourseList() {
-
-    return (
-        <React.Fragment>
-            <div overflow="auto">
-                    <Paper>
-                        <Table aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Dessert (100g serving)</TableCell>
-                                    <TableCell align="right">Calories</TableCell>
-                                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {rows.map(row => (
-                                    <TableRow key={row.name}>
-                                        <TableCell component="th" scope="row">
-                                            {row.name}
-                                        </TableCell>
-                                        <TableCell align="right">{row.calories}</TableCell>
-                                        <TableCell align="right">{row.fat}</TableCell>
-                                        <TableCell align="right">{row.carbs}</TableCell>
-                                        <TableCell align="right">{row.protein}</TableCell>
+class MyCourseList extends React.Component {
+    render() {
+        const {classes} = this.props;
+        return (
+            <React.Fragment>
+                
+                        <Paper className={classes.tableWrapper}>
+                            <Table stickyHeader aria-label="sticky table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Dessert (100g serving)</TableCell>
+                                        <TableCell align="right">Calories</TableCell>
+                                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
                                     </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </Paper>
-                    </div>
-        </React.Fragment>
-
-
-    );
+                                </TableHead>
+                                <TableBody>
+                                    {rows.map(row => (
+                                        <TableRow key={row.name}>
+                                            <TableCell component="th" scope="row">
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell align="right">{row.calories}</TableCell>
+                                            <TableCell align="right">{row.fat}</TableCell>
+                                            <TableCell align="right">{row.carbs}</TableCell>
+                                            <TableCell align="right">{row.protein}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Paper>
+            </React.Fragment>
+    
+    
+        );
+    }
+    
 }
+export default withStyles(UserDashboardStyles.styles, { withTheme: true })(MyCourseList);
