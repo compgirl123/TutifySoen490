91c0b979f175848a5c1f519bf6f766d3083b3018
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2019-11-03 14:57:10 -0500

    #72 layout of userdashboard is fixed

--- tutify/src/styles/UserDashboard/UserDashboard-styles.js
+++ tutify/src/styles/UserDashboard/UserDashboard-styles.js
@@ -1,22 +1,17 @@
 
 export const styles = theme => ({
     root: {
-        flexGrow: 1,
         paddingTop: theme.spacing(10),
         paddingBottom: theme.spacing(8),
-        marginLeft:'200px'
+        paddingLeft: theme.spacing(27),
+        overflow: 'hidden'
       },
       drawer: {
-          width: 240,
-          flexShrink: 0,
+          anchor: 'left',
       },
       toolbar: theme.mixins.toolbar,
       gridItem: {
           padding: theme.spacing(1),
-      },
-      cardWrapper: {
-        maxHeight:400,
-        overflow: 'auto',
       },
       tableWrapper: {
         maxHeight:400,
