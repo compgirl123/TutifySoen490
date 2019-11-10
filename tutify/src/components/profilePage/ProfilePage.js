2458ffd0ee8e4c9e32e45218268fd7f84a128f23
Bilal101 <35821680+Bilal101@users.noreply.github.com>
2019-11-09 23:39:34 -0500

    removed  unnecessary code from profilepage.js #77

--- tutify/src/components/profilePage/ProfilePage.js
+++ tutify/src/components/profilePage/ProfilePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
 import * as tutifyStyle from '../../styles/ProfilePage-styles';
 import { withStyles } from "@material-ui/core/styles";
 import Container from '@material-ui/core/Container';
@@ -7,10 +6,7 @@ import Grid from '@material-ui/core/Grid';
 import Footer from "../Footer";
 import DashBoardNavBar from "./DashBoardNavBar";
 import UserInfo from './UserInfo';
-import TutorInfo from '../TutorProfile/TutorInfo'
-
-import Card from '@material-ui/core/Card';
-
+​import Card from '@material-ui/core/Card';
 
 class ProfilePage extends React.Component {
   constructor(props) {
@@ -21,17 +17,17 @@ class ProfilePage extends React.Component {
       __t: ""
     };
   }
-
+​
   toggleDrawer = booleanValue => () => {
     this.setState({
       drawerOpened: booleanValue
     });
   };
-
+​
   componentDidMount() {
     this.checkSession();
   }
-
+​
   checkSession = () => {
     fetch('http://localhost:3001/api/checkSession', {
       method: 'GET',
@@ -49,7 +45,7 @@ class ProfilePage extends React.Component {
       })
       .catch(err => console.log(err));
   };
-
+​
   handleChange(event) {
     fetch('http://localhost:3001/api/logout', {
       method: 'GET',
@@ -58,14 +54,14 @@ class ProfilePage extends React.Component {
       .then(response => response.json())
       .then(res => {
         this.setState({ Toggle: false });
-
+​
       })
       .catch(err => console.log(err));
   };
-
+​
   render() {
     const { classes } = this.props;
-
+​
     return (
       <React.Fragment>
         <main>
@@ -73,43 +69,28 @@ class ProfilePage extends React.Component {
           <main className={classes.content}>
             <div className={classes.appBarSpacer} />
             <Container maxWidth="lg" className={classes.container}>
-              <Typography component="h6" variant="h6" align="center" color="textPrimary" gutterBottom>
-                My Profile
-              </Typography>
+              
               <Grid container spacing={4}>
-
+​
                 {/* User Info */}
                 <Grid item xs={5}>
                   <Card>
-                  {this.state.__t === "tutor" ? 
-                 <TutorInfo/>
-                 :
-                <UserInfo /> }
+                 
+                <UserInfo /> 
                   </Card>
                  
                 </Grid>
-                <Grid item xs={12} md={6} lg={6}>
-                  {/* Adding Picture */}
-                  {this.state.__t === "tutor"
-                    ? <img src={this.state.tutorPicture} alt="Profile" width="225" height="225">
-                    </img>
-                    : this.state.__t === "student"
-                      ? <img src={"https://i.imgur.com/L6lDhbz.jpg"} alt="Profile" width="225" height="225">
-                      </img>
-                      :
-                      <p></p>
-                  }
-      </Grid>
+                
               </Grid>
             </Container>
             <main>
               {/* Hero unit */}
-
+​
             </main>
             <Footer />
-
+​
           </main>
-
+​
         </main>
       </React.Fragment>
     );
