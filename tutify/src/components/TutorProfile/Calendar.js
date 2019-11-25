29ee1c4c7de2909b0b8f630119c72ee08284b60e
tanyamultani <tanyamultani1@gmail.com>
2019-11-24 19:38:02 -0500

    #102 removed usless imports

--- tutify/src/components/TutorProfile/Calendar.js
+++ tutify/src/components/TutorProfile/Calendar.js
@@ -7,24 +7,19 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import Calendar from 'react-calendar'
-import AddToCalendar from 'react-add-to-calendar';
 import Grid from '@material-ui/core/Grid';
 import AddIcon from '@material-ui/icons/Add';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import Button from '@material-ui/core/Button';
-import swal from 'sweetalert';
 import TextField from '@material-ui/core/TextField';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
-import axios from 'axios';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Fab from '@material-ui/core/Fab';
-import TimePicker from 'react-time-picker';
 
 class NewCalendar extends React.Component {
     constructor(props) {
