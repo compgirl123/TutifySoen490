21d4af6f6ff50ec9fa51aa707f6c1bdbd5565ab3
compgirl123 <claudia.f.feochari@hotmail.com>
2019-12-26 21:57:53 -0500

    #110 : Added my courses tests for both students and tutors

---
+++ tutify/test/mycourses-tests-students.spec.js
@@ -0,0 +1,69 @@
+import React from "react";
+import MyCourses, { MyCourses as MyCoursesClass } from "../src/components/ProfilePage/MyCourses";
+import ViewCourse, { ViewCourse as ViewCourseClass } from "../src/components/StudentCourseView/ViewCourse";
+import ContactTutor from "../src/components/StudentCourseView/ContactTutor";
+import Documents from "../src/components/StudentCourseView/Documents";
+import { createMount } from '@material-ui/core/test-utils';
+import { configure } from 'enzyme';
+import Adapter from "enzyme-adapter-react-16";
+import { shallow } from 'enzyme';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+// importing the json object with the profile information
+var json = require("./testDb/profiles.json");
+
+configure({ adapter: new Adapter() });
+
+describe('The Students Courses Page', () => {
+    let mount;
+
+    beforeAll(() => {
+        mount = createMount();
+    });
+
+    it('Testing the student courses for Students. Checking if all of the required information for student course page exists on the page.', () => {
+        // All the mounting and state setting
+        const wrapper = mount(<MyCourses></MyCourses>);
+        const wrapper_shallow = shallow(<MyCourses></MyCourses>);
+        const student_class_wrapper = wrapper.find(MyCoursesClass);
+        student_class_wrapper.setState({ data: json.data });
+
+        /**
+         * Checking the two student courses page (general and specific course page) and see if all elements are present 
+        */
+
+        // Finding the Typography component that contains the Main Title Header Present on the Page.
+        const course_title = wrapper_shallow.dive().find(Typography).at(0);
+        // Make sure the header name for the student page has the value My Enrolled Courses
+        expect(course_title.props().children).toBe("My Enrolled Courses");
+
+        // Go to next page which contains information about a particular course the student is registered in.
+        const wrapper_shallow_specific_course = shallow(<ViewCourse></ViewCourse>);
+
+        // Finding the Grid component containing the Different classes the student is registered for.
+        const test = wrapper_shallow_specific_course.dive().find(Grid).at(0);
+        // Seeing if the Grid components Exists (import grid)
+        expect(test.exists()).toBeTruthy();
+
+        // Finding the Typography component that contains the title for the Contact Information component.
+        const contact_tutor_title = wrapper_shallow_specific_course.dive().find(Typography).at(0);
+        // Make sure the specific student course page contains tutor Contact Info header on it.
+        expect(contact_tutor_title.props().children).toBe("Contact Info");
+
+        // Finding the ContactTutor component that contains the information for the ContactTutor component.
+        const contact_tutor_component = wrapper_shallow_specific_course.dive().find(ContactTutor).at(0);
+        // Make sure the specific student course page contains the component <ContactTutor />
+        expect(contact_tutor_component.exists()).toBeTruthy();
+
+        // Finding the Typography component that contains the title for the Course Documents component.
+        const course_title2 = wrapper_shallow_specific_course.dive().find(Typography).at(1);
+        // Make sure the specific student course page contains tutor Course Documents header on it.
+        expect(course_title2.props().children).toBe("Course Documents");
+        // Finding the Documents component that contains the information for the Documents component.
+        const document_component = wrapper_shallow_specific_course.dive().find(Documents).at(0);
+        // Make sure the specific student course page contains the component <Documents/>
+        expect(document_component.exists()).toBeTruthy();
+    
+    });
+}); 
---
+++ tutify/test/mycourses-tests-tutors.spec.js
@@ -0,0 +1,64 @@
+import React from "react";
+import TutorCourses, { TutorCourses as TutorCoursesClass } from "../src/components/TutorProfile/TutorCourses";
+import TutorCourseView, { TutorCourseView as TutorCourseViewClass } from "../src/components/TutorCourseView/TutorCourseView";
+import CourseStudents from "../src/components/TutorCourseView/CourseStudents";
+import UploadDoc from "../src/components/TutorCourseView/UploadDoc";
+import { createMount } from '@material-ui/core/test-utils';
+import { configure } from 'enzyme';
+import Adapter from "enzyme-adapter-react-16";
+import { shallow } from 'enzyme';
+import Typography from '@material-ui/core/Typography';
+
+// importing the json object with the profile information
+var json = require("./testDb/profiles.json");
+
+configure({ adapter: new Adapter() });
+
+describe('The Tutors Courses Page', () => {
+    let mount;
+
+    beforeAll(() => {
+        mount = createMount();
+    });
+
+    it('Testing the update courses for Students. Updating one of the values present in the Dialog Box (education level of student)', () => {
+        // All the mounting and state setting
+        const wrapper = mount(<TutorCourses></TutorCourses>);
+        const wrapper_shallow = shallow(<TutorCourses></TutorCourses>);
+        const student_class_wrapper = wrapper.find(TutorCoursesClass);
+        student_class_wrapper.setState({ data: json.data });
+
+        /**
+         * Checking the two tutor courses page (general and specific course page) and see if all elements are present 
+        */
+
+         // Finding the Typography component that contains the Main Title Header Present on the Page.
+        const course_title = wrapper_shallow.dive().find(Typography).at(0);
+        // Make sure the header name for the student page has the value Courses Currently Taught
+        expect(course_title.props().children).toBe("Courses Currently Taught");
+
+        // Go to next page which contains information about a particular course the student is registered in.
+        const tutor_course_view_wrapper = shallow(<TutorCourseView></TutorCourseView>);
+
+        // Finding the Typography component that contains the title for the Contact Information component.
+        const students_title = tutor_course_view_wrapper.dive().find(Typography).at(0);
+        // Make sure the specific tutor course page contains the header Students as it contains a table with students.
+        expect(students_title.props().children).toBe("Students");
+
+        // Finding the CourseStudents component that contains the information for the CourseStudents component.
+        const course_students_tutor_component = tutor_course_view_wrapper.dive().find(CourseStudents).at(0);
+        // Make sure the specific student course page contains the component <CourseStudents />
+        expect(course_students_tutor_component.exists()).toBeTruthy();
+
+        // Finding the Typography component that contains the information for the Typography component.
+        const documents_uploaded = tutor_course_view_wrapper.dive().find(Typography).at(1);
+        // Make sure the specific student course page contains tutor Documents Uploaded header on it.
+        expect(documents_uploaded.props().children).toBe("Documents Uploaded");
+
+        // Finding the UploadDoc component that contains the information for the UploadDoc component.
+        const document_component = tutor_course_view_wrapper.dive().find(UploadDoc).at(0);
+        // Make sure the specific student course page contains the component <UploadDoc/>
+        expect(document_component.exists()).toBeTruthy();
+    
+    });
+}); 
