0f03a0357e2567484efcfbaf87f8fa1fe507a9f7
compgirl123 <claudia.f.feochari@hotmail.com>
2019-12-27 17:37:31 -0500

    #110 : Added notification dashboard tests

--- tutify/test/dashboard-tests-students.spec.js
+++ tutify/test/dashboard-tests-students.spec.js
@@ -3,11 +3,14 @@ import TodoItem, { TodoItem as TodoItemClass } from "../src/components/UserDashb
 import TodoList, { TodoList as TodoListClass } from "../src/components/UserDashboardPage/Todo/TodoList";
 import Todos, { Todos as TodosClass } from "../src/components/UserDashboardPage/Todo/Todos";
 import AddTodo, { AddTodo as AddTodoClass } from "../src/components/UserDashboardPage/Todo/AddTodo";
+import Notifications, { Notifications as NotificationsClass } from "../src/components/UserDashboardPage/Notifications";
 import { createMount } from '@material-ui/core/test-utils';
 import { configure } from 'enzyme';
 import Adapter from "enzyme-adapter-react-16";
 import { shallow } from 'enzyme';
 import TextField from '@material-ui/core/TextField';
+import TableCell from '@material-ui/core/TableCell';
+import List from '@material-ui/core/List';
 
 // importing the json object with the profile information
 var json = require("./testDb/enhanced-profile.json");
@@ -30,8 +33,8 @@ describe('The Student Dashboard Page To Do List', () => {
 
         // The value sent in the input
         const toDoItem = { target: { value: "Do COMP 472 HomeWork"} } 
-        const input = wrapper.find(TextField).at(0);
-        input.props().onChange(toDoItem);
+        const toDo = wrapper.find(TextField).at(0);
+        toDo.props().onChange(toDoItem);
 
         expect(student_class_wrapper.state().title).toBe("Do COMP 472 HomeWork");
 
@@ -62,18 +65,85 @@ describe('The Student Dashboard Page To Do List', () => {
         wrapper_shallow.setProps({ sessionTodos: { todos: toDoFullInfo } });
 
         // Getting the Todo Component on the Page that should contain the TodoList.
-        const input = wrapper.find(Todos).at(0);
+        const toDoComponent = wrapper.find(Todos).at(0);
 
         // Expect the title of the todo item to be a certain value
-        expect(input.props().todos[0].title).toBe("Do COMP 472 HomeWork");
+        expect(toDoComponent.props().todos[0].title).toBe("Do COMP 472 HomeWork");
 
         // Expect the checkbox of the todo item to not be checked
-        expect(input.props().todos[0].completed).toBe(false);
+        expect(toDoComponent.props().todos[0].completed).toBe(false);
 
     });
 }); 
 
 describe('The Student Dashboard Page Notifications', () => {
-    // TO DO
+    let mount;
+
+    beforeAll(() => {
+        mount = createMount();
+    });
+    
+
+    it('Add To Do Item to List', () => {
+
+        const notificationsArray = [
+            {
+              _id: {
+                $oid: "5e01689fbd1ef084d89dec5b"
+              },
+              title: "A1",
+              text: "New announcement.",
+              tutorImg: "https://i.imgur.com/kyh5A1e.jpg",
+              tutorName: "Mohammed Alawami",
+              tutorid: {
+                $oid: "5dacd1cf1c9d440000aa0b1b"
+              }
+            },
+            {
+              _id: {
+                $oid: "5e0287e3641f2eac8f2837ae"
+              },
+              title: "Testing",
+              text: "Hello Students",
+              tutorImg: "https://i.imgur.com/kyh5A1e.jpg",
+              tutorName: "Mohammed Alawami",
+              tutorid: {
+                $oid: "5dacd1cf1c9d440000aa0b1b"
+              }
+            }
+          ]
+        // All the mounting and state setting
+        const wrapper = mount(<Notifications notifications ={notificationsArray}></Notifications>);
+        const wrapper_shallow = shallow(<Notifications notifications ={notificationsArray}></Notifications>);
+        const student_class_wrapper = wrapper.find(NotificationsClass);
+        student_class_wrapper.setState({ notifications: notificationsArray });
+
+        const notifications_title = wrapper.find(TableCell).at(0);
+        expect(notifications_title.props().children.props.children).toBe("Notifications");
+
+        /* Checking if appropriate notifications are displayed on the student's side. */
+        // Getting Notification Information. Latest notification added appears first.
+        const notification = wrapper.find(List).at(0);
+
+        // Expect Title to equal title value of newest notification posted by tutor to student.
+        expect(notification.props().children[0].props.notif.title).toBe("Testing");
+
+        // Expect Text to equal text value of newest notification posted by tutor to student.
+        expect(notification.props().children[0].props.notif.text).toBe("Hello Students");
+
+        // Expect Tutor name to equal tutor name of newest notification posted by tutor to student.
+        expect(notification.props().children[0].props.notif.tutorName).toBe("Mohammed Alawami");
+
+
+        // Expect Title to equal title value of older notification posted by tutor to student.
+        expect(notification.props().children[1].props.notif.title).toBe("A1");
+
+        // Expect Text to equal text value of older notification posted by tutor to student.
+        expect(notification.props().children[1].props.notif.text).toBe("New announcement.");
+
+        // Expect Tutor name to equal tutor name of older notification posted by tutor to student.
+        expect(notification.props().children[1].props.notif.tutorName).toBe("Mohammed Alawami");
+
+    });
 
 }); 
