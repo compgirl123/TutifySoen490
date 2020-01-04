3ae17854ba1e6253fc70a10c14c47de5e25cf03b
compgirl123 <claudia.f.feochari@hotmail.com>
2020-01-03 20:24:17 -0500

    #102 : Fixed calendar tutor dashboard widget code formatting

--- tutify/src/components/TutorProfile/Calendar.js
+++ tutify/src/components/TutorProfile/Calendar.js
@@ -22,222 +22,222 @@ import CardContent from '@material-ui/core/CardContent';
 import Fab from '@material-ui/core/Fab';
 
 class NewCalendar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          drawerOpened: false,
-          title: "",
-          description: "",
-          location: "",
-          date: "",
-          startTime: "",
-          endTime: "",
-          open: false,
-          scroll: 'paper',
-        };
-        this.handleClickOpen = this.handleClickOpen.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-      }
-    
-      toggleDrawer = booleanValue => () => {
-        this.setState({
-          drawerOpened: booleanValue
-        });
-      };
-    
-      handleFeedback = () => {
-        this.setState({ open: true })
-      }
-    
-      handleClickOpen = () => {
-        this.setState({ open: true });
-      };
-    
-      handleClose = () => {
-        this.setState({ open: false });
-      };
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawerOpened: false,
+      title: "",
+      description: "",
+      location: "",
+      date: "",
+      startTime: "",
+      endTime: "",
+      open: false,
+      scroll: 'paper',
+    };
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  toggleDrawer = booleanValue => () => {
+    this.setState({
+      drawerOpened: booleanValue
+    });
+  };
+
+  handleFeedback = () => {
+    this.setState({ open: true })
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
 
-      componentDidMount() {
-        this.checkSession();
-      }
-    
-     
-    
-      checkSession = () => {
-        fetch('http://localhost:3001/api/checkSession', {
-          method: 'GET',
-          credentials: 'include'
-        })
-          .then(response => response.json())
-          .then((res) => {
-           
-            
-          })
-          .catch(err => console.log(err));
-      };
-    
-    render() {
-        const { classes } = this.props;
-        const { open } = this.state;
 
-        return (
-            <React.Fragment>
-        <Card className={classes.card}>                    
-        <Table stickyHeader aria-label="">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell><Typography variant="h6">Calendar</Typography></TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                        <CardContent >
-                        <Grid container spacing={2}>
+  componentDidMount() {
+    this.checkSession();
+  }
 
-                        <Grid item xs={8}>
-<Calendar 
-onChange = {date => this.setState({ date })}
-          value={this.state.date}/>
-</Grid>
-<Grid item xs={4}>
 
-<Fab variant="extended" aria-label="edit"
-              justify="center"
-              onClick={() => { this.handleClickOpen(); }}
-              >
-                                <AddIcon />
 
-              Add Event
+  checkSession = () => {
+    fetch('http://localhost:3001/api/checkSession', {
+      method: 'GET',
+      credentials: 'include'
+    })
+      .then(response => response.json())
+      .then((res) => {
+
+
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { open } = this.state;
+
+    return (
+      <React.Fragment>
+        <Card className={classes.card}>
+          <Table stickyHeader aria-label="">
+            <TableHead>
+              <TableRow>
+                <TableCell><Typography variant="h6">Calendar</Typography></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <CardContent >
+                <Grid container spacing={2}>
+
+                  <Grid item xs={8}>
+                    <Calendar
+                      onChange={date => this.setState({ date })}
+                      value={this.state.date} />
+                  </Grid>
+                  <Grid item xs={4}>
+
+                    <Fab variant="extended" aria-label="edit"
+                      justify="center"
+                      onClick={() => { this.handleClickOpen(); }}
+                    >
+                      <AddIcon />
+
+                      Add Event
             </Fab>
-            </Grid>
-            </Grid>
+                  </Grid>
+                </Grid>
 
-</CardContent>
-                            
-                        </TableBody>
-                    </Table>
-                    <div>
+              </CardContent>
 
-<Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open={open}>
-  <DialogTitle id="simple-dialog-title">Add an Event</DialogTitle>
-  <DialogContent>
-    <DialogContentText>
-      To add an event, fill out the desired value fields and click save.
+            </TableBody>
+          </Table>
+          <div>
+
+            <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open={open}>
+              <DialogTitle id="simple-dialog-title">Add an Event</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  To add an event, fill out the desired value fields and click save.
     </DialogContentText>
 
-    <TextField
-       InputLabelProps={{
-        shrink: true,
-      }}
-      margin="dense"
-      id="title"
-      name="title"
-      onChange={e => this.setState({ title: e.target.value })}
-      autoComplete="title"
-      label="Title"
-      type="title"
-      fullWidth
-    />
+                <TextField
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  margin="dense"
+                  id="title"
+                  name="title"
+                  onChange={e => this.setState({ title: e.target.value })}
+                  autoComplete="title"
+                  label="Title"
+                  type="title"
+                  fullWidth
+                />
 
-    <TextField
-       InputLabelProps={{
-        shrink: true,
-      }}
-      margin="dense"
-      id="description"
-      name="description"
-      onChange={e => this.setState({ description: e.target.value })}
-      autoComplete="description"
-      label="Description"
-      type="description"
-      fullWidth
-    />
+                <TextField
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  margin="dense"
+                  id="description"
+                  name="description"
+                  onChange={e => this.setState({ description: e.target.value })}
+                  autoComplete="description"
+                  label="Description"
+                  type="description"
+                  fullWidth
+                />
 
-    <TextField
-       InputLabelProps={{
-        shrink: true,
-      }}
-      margin="dense"
-      id="location"
-      name="location"
-      onChange={e => this.setState({ location: e.target.value })}
-      autoComplete="location"
-      label="Location"
-      type="location"
-      fullWidth
-    />
-
-
-    <TextField
-     InputLabelProps={{
-      shrink: true,
-    }}
-      margin="dense"
-      id="date"
-      name="date"
-      label="Date"
-      onChange={e => this.setState({ date: e.target.value })}
-      type="date"
-      fullWidth
-    /> 
-
-     <TextField
-           margin="dense"
-
-     InputLabelProps={{
-      shrink: true,
-    }}
-      id="startTime"
-      name="startTime"
-      label="Start Time"
-      onChange={e => this.setState({ startTime: e.target.value })}
-      type="time"
-      fullWidth
-      
-    /> 
+                <TextField
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  margin="dense"
+                  id="location"
+                  name="location"
+                  onChange={e => this.setState({ location: e.target.value })}
+                  autoComplete="location"
+                  label="Location"
+                  type="location"
+                  fullWidth
+                />
 
 
-   <TextField
-         margin="dense"
+                <TextField
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  margin="dense"
+                  id="date"
+                  name="date"
+                  label="Date"
+                  onChange={e => this.setState({ date: e.target.value })}
+                  type="date"
+                  fullWidth
+                />
 
-     InputLabelProps={{
-      shrink: true,
-    }}
-      id="endTime"
-      name="endTime"
-      label="End Time"
-      onChange={e => this.setState({ endTime: e.target.value })}
-      type="time"
-      fullWidth
-    /> 
+                <TextField
+                  margin="dense"
 
-  </DialogContent>
-  <Grid
-    container
-    direction="row-reverse"
-    justify="space-between"
-    alignItems="baseline"
-  >
-    <Grid item>
-      <DialogActions>
-        <Button onClick={this.handleClose}>Close</Button>
-      </DialogActions>
-    </Grid>
-    <Grid item>
-      <DialogActions>
-        <Button onClick={this.addEvent}>Save</Button>
-      </DialogActions>
-    </Grid>
-  </Grid>
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  id="startTime"
+                  name="startTime"
+                  label="Start Time"
+                  onChange={e => this.setState({ startTime: e.target.value })}
+                  type="time"
+                  fullWidth
 
-</Dialog>
-</div>
+                />
 
 
-                </Card>
-            </React.Fragment>
-        );
-    }
+                <TextField
+                  margin="dense"
+
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  id="endTime"
+                  name="endTime"
+                  label="End Time"
+                  onChange={e => this.setState({ endTime: e.target.value })}
+                  type="time"
+                  fullWidth
+                />
+
+              </DialogContent>
+              <Grid
+                container
+                direction="row-reverse"
+                justify="space-between"
+                alignItems="baseline"
+              >
+                <Grid item>
+                  <DialogActions>
+                    <Button onClick={this.handleClose}>Close</Button>
+                  </DialogActions>
+                </Grid>
+                <Grid item>
+                  <DialogActions>
+                    <Button onClick={this.addEvent}>Save</Button>
+                  </DialogActions>
+                </Grid>
+              </Grid>
+
+            </Dialog>
+          </div>
+
+
+        </Card>
+      </React.Fragment>
+    );
+  }
 }
 export default withStyles(UserDashboardStyles.styles, { withTheme: true })(NewCalendar);
