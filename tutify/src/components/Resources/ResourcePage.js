48a45b4479d7fcc3e120b8bde33f50c51c1e1d46
Cynthia <cynthia.cherrier@hotmail.com>
2020-01-11 15:19:25 -0500

    #124 refactored to combine 3 components into one

--- tutify/src/components/Resources/ResourcePage.js
+++ tutify/src/components/Resources/ResourcePage.js
@@ -5,13 +5,18 @@ import * as ResourcesStyles from '../../styles/Resources-styles';
 import { withStyles } from "@material-ui/core/styles";
 import Typography from '@material-ui/core/Typography';
 import PostSecResourceNavigation from './PostSecResourceNavigation';
+import PrimaryResourceNavigation from './PrimaryResourceNavigation';
+import SecondaryResourceNavigation from './SecondaryResourceNavigation';
 
 
 class PostSecondaryResource extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            Toggle: false,
+            primary: false,
+            secondary: false,
+            postsecondary: false,
         };
     }
 
@@ -29,12 +34,7 @@ class PostSecondaryResource extends React.Component {
                 if (res.isLoggedIn) {
                     this.setState({
                         Toggle: true,
-                        _id: res.userInfo._id,
-                        todos: res.userInfo.todos,
-                        tutors: res.userInfo.tutors,
-                        notifications: res.userInfo.notifications
                     });
-                    this.getDataFromDb()
                 }
                 else {
                     this.setState({ Toggle: false });
@@ -43,9 +43,20 @@ class PostSecondaryResource extends React.Component {
             .catch(err => console.log(err));
     };
 
+    imgForLevel = () => {
+        if(this.props.location.state.primary)
+            return 'https://www.clipartwiki.com/clipimg/full/257-2579818_teacher-clip-early-childhood-kids-learn-cartoon-png.png'
+        if(this.props.location.state.secondary)
+            return 'https://24t9d72kcs873my15o9hr1pu-wpengine.netdna-ssl.com/wp-content/uploads/2017/09/student-productivity-toggl-702x526.jpg'
+        if(this.props.location.state.postsecondary)
+            return 'https://24t9d72kcs873my15o9hr1pu-wpengine.netdna-ssl.com/wp-content/uploads/2017/09/student-productivity-toggl-702x526.jpg'
+    }
+
 
     render() {
         const { classes } = this.props;
+        const { primary, secondary, postsecondary } = this.props.location.state;
+
         return (
             <React.Fragment>
                 <DashBoardNavBar />
@@ -54,9 +65,11 @@ class PostSecondaryResource extends React.Component {
                         Resources and Tools
                     </Typography>
                     <div className={classes.logo}>
-                        <img alt="logo" src={'https://24t9d72kcs873my15o9hr1pu-wpengine.netdna-ssl.com/wp-content/uploads/2017/09/student-productivity-toggl-702x526.jpg'} />
+                        <img alt="logo" src={this.imgForLevel()} />
                     </div>
-                    <PostSecResourceNavigation />
+                    {primary ?  <PrimaryResourceNavigation /> : <></>}
+                    {secondary ?  <PostSecResourceNavigation /> : <></>}
+                    {postsecondary ?  <SecondaryResourceNavigation /> : <></>}
                 </div>
                 <Footer />
             </React.Fragment>
