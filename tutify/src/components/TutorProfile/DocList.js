39fbc280ac243630f0951589ff96b9de03717e75
kasthurie <sriahila@hotmail.com>
2020-01-11 17:00:25 -0500

    #117 made the icons centered in the table

--- tutify/src/components/TutorProfile/DocList.js
+++ tutify/src/components/TutorProfile/DocList.js
@@ -423,11 +423,11 @@ getCourses = () => {
                           <td>COMP 472</td>
 
                           <td>{uploadDate}</td>
-                          <td><Button type="button" variant="contained" className="submit" size="small" onClick={() => window.open("http://localhost:3000/tutorCourses?"+encrypted_file_name)} id={file._id}><MenuBookIcon/></Button></td>
-                          <td><Button type="button" variant="contained" className="submit" size="small"  onClick={() => window.open("http://localhost:3000/students")} id={file._id}><GroupAddIcon/></Button></td>
+                          <td align="center"><Button type="button" variant="contained" className="submit" size="small" onClick={() => window.open("http://localhost:3000/tutorCourses?"+encrypted_file_name)} id={file._id}><MenuBookIcon/></Button></td>
+                          <td align="center"><Button type="button" variant="contained" className="submit" size="small"  onClick={() => window.open("http://localhost:3000/students")} id={file._id}><GroupAddIcon/></Button></td>
 
-                          <td><Button type="button" variant="contained" className="submit" size="small" onClick={() => window.open(link, "_blank")} id={file._id}><GetAppIcon/></Button></td>
-                          <td><Button type="button" variant="contained" className="submit" size="small" onClick={e => this.deleteListItem()} ><DeleteIcon/></Button></td>
+                          <td align="center"><Button type="button" variant="contained" className="submit" size="small" onClick={() => window.open(link, "_blank")} id={file._id}><GetAppIcon /></Button></td>
+                          <td align="center"><Button type="button" variant="contained" className="submit" size="small" onClick={e => this.deleteListItem()} ><DeleteIcon/></Button></td>
             
                         </TableRow>
                       )
