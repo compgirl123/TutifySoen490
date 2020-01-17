653fa21676d459154a0df59cab5413d03dffef8e
kasthurie <sriahila@hotmail.com>
2020-01-16 19:15:58 -0500

    #133 Page reloads after a delete

--- tutify/src/components/TutorProfile/DocList.js
+++ tutify/src/components/TutorProfile/DocList.js
@@ -51,7 +51,7 @@ class DocList extends React.Component {
       .catch(err => console.log(err));
   }
 
-  // tutor deletes a documents from files list
+  //tutor deletes a documents from files list
   getSelectedFiletoDelete(event,encrypted_file_name) {
     swal({
       title: "Are you sure you want delete this document?",
@@ -67,7 +67,8 @@ class DocList extends React.Component {
         file_id: encrypted_file_name
     }
     ).then((res) => {})
-      .catch(err => console.log(err));}
+      .catch(err => console.log(err));
+      window.location.reload();}
   });
   }
 
