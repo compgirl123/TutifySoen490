6404c7de2491d9382b175cfdbbd693daf6866bf0
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2020-01-23 16:59:20 -0500

    #143 cleaned up style sheet by removing unused code

--- tutify/src/styles/UploadDocuments-styles.js
+++ tutify/src/styles/UploadDocuments-styles.js
@@ -15,16 +15,6 @@ export const theme = createMuiTheme({
 
   
 export const styles = theme => ({
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    width: 400,
-    margin: "auto",
-  },
-  menu: {
-    marginTop: '10px',
-  },
   container: {
     marginTop: '30px',
   },
@@ -38,54 +28,9 @@ export const styles = theme => ({
   icon: {
     marginRight: theme.spacing(2),
   },
-  notchedOutline: {
-    borderColor: '#9E9E9E !important',
-  },
   heroContent: {
     background: '#EEEEEE',
     padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-
-  chip: {
-    background: 'linear-gradient(45deg, rgba(0,200,83,1) 0%, rgba(200,255,75,1) 100%)', 
-    fontWeight:"bold",
-    margin: theme.spacing(0.5),
-    marginTop: theme.spacing(1),
-  },
-  gridContainer: {
-    background: theme.palette.background.paper,
-  },
-  school: {
-    color: '#969696',
-  },
-  program_of_study: {
-    color: '#8ae600',
-  },
-  bigAvatar: {
-    margin: 'auto',
-    width: 100,
-    height: 100,
-  },
-  dialogTitle: {
-    flexGrow: 1,
   },
   paper: {
     padding: theme.spacing(2),
@@ -117,8 +62,5 @@ export const styles = theme => ({
     cursor: "pointer",
     fontSize: "20px",
     background: 'linear-gradient(45deg, rgba(0,200,83,1) 0%, rgba(200,255,75,1) 100%)' ,
-  },
-  connect: {
-    
   },
 });
