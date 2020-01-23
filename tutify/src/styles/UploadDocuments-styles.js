9698a0d7b1db05442ed0e1bfb8d7a90b1936c941
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2020-01-23 18:20:39 -0500

    #143 fixed file upload frontend

--- tutify/src/styles/UploadDocuments-styles.js
+++ tutify/src/styles/UploadDocuments-styles.js
@@ -1,19 +1,3 @@
-import { createMuiTheme } from "@material-ui/core/styles";
-
-export const theme = createMuiTheme({
-    overrides: {
-      MuiInputLabel: { // Name of the component ⚛️ / style sheet
-        root: { // Name of the rule
-          color: "#9E9E9E ",
-          "&$focused": { // increase the specificity for the pseudo class
-            color: "#9E9E9E"
-          },
-        }
-      }
-    }
-  });
-
-  
 export const styles = theme => ({
   container: {
     marginTop: '30px',
@@ -30,37 +14,35 @@ export const styles = theme => ({
   },
   heroContent: {
     background: '#EEEEEE',
-    padding: theme.spacing(8, 0, 6),
+    padding: theme.spacing(8, 0, 4),
   },
   paper: {
     padding: theme.spacing(2),
     margin: 'auto',
     maxWidth: 500,
   },
-  uploadDocApp:{
+  uploadDocApp: {
     textAlign: 'center'
   },
   button: {
     margin: theme.spacing(1)
   },
-  AppTable: {
-    width: "70%",
-    margin: "10px auto",
-    borderCollapse:  "collapse"
-  },
-  AppTableTr: {
-    border: "1px solid black"
-  },
-  AppHeader:{
+  AppHeader: {
     height: "75px",
     padding: "20px",
   },
-  inputUpload:{
-    border: "1px solid #ccc",
-    display: "inline-block",
-    padding: "6px 12px",
-    cursor: "pointer",
-    fontSize: "20px",
-    background: 'linear-gradient(45deg, rgba(0,200,83,1) 0%, rgba(200,255,75,1) 100%)' ,
+  submit: {
+    float:'right',
+  },
+  uploadContainer: {
+    display: 'inline-block',
+    textAlign: 'left',
+    background: '#fff',
+    padding: '10px',
+    width: '450px',
+    position: 'relative,',
+  },
+  seeMore: {
+    marginTop:'10px',
   },
 });
