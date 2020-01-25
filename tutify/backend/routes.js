b7dfe2f9f5f4203a466dfa605fea07c213ecc0ef
Bilal101 <35821680+Bilal101@users.noreply.github.com>
2020-01-25 01:32:07 -0500

    route to add course #142

--- tutify/backend/routes.js
+++ tutify/backend/routes.js
@@ -84,6 +84,8 @@ router.delete('/deleteAppointment', appointmentController.deleteAppointment);
 
 router.get('/getCourses', courseController.getCourses);
 
+router.post('/addCourseToDb', courseController.addCourseToDb);
+
 // -------- Files ROUTES --------- // 
 
 router.get('/getFile/:filename', filesController.getFile);
