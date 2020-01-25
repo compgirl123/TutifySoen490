25a2a84730942ad0e66ef002c2169633fc89e9a3
Pierre Watine <watinepierre@gmail.com>
2020-01-24 23:15:10 -0500

    Logging on account controller

--- tutify/backend/controllers/account.controller.js
+++ tutify/backend/controllers/account.controller.js
@@ -3,16 +3,24 @@ const Account = require('../models/models').Account;
 // this method fetches all available accounts in our database
 exports.getAccount = async function (req, res) {
     Account.find((err, data) => {
-      if (err) return res.json({ success: false, error: err });
-      return res.json({ success: true, data: data });
+        if (err) {
+            console.error("Could not find the account");
+            return res.json({ success: false, error: err });
+        }
+        console.info("Account found");
+        return res.json({ success: true, data: data });
     });
 }
 
 // this method overwrites existing account in our database
-exports.updateAccount = async function (req, res)  {
+exports.updateAccount = async function (req, res) {
     const { id, update } = req.body;
     Account.findByIdAndUpdate(id, update, (err) => {
-        if (err) return res.json({ success: false, error: err });
+        if (err) {
+            console.error("Could not update the account, account not found");
+            return res.json({ success: false, error: err });
+        }
+        console.info("Account is updated");
         return res.json({ success: true });
     });
 };
@@ -24,15 +32,20 @@ exports.putAccount = async function (req, res) {
     const { id, message } = req.body;
 
     if ((!id && id !== 0) || !message) {
+        console.error("Missing account id");
         return res.json({
-        success: false,
-        error: 'INVALID INPUTS',
+            success: false,
+            error: 'INVALID INPUTS',
         });
     }
     data.message = message;
     data.id = id;
     data.save((err) => {
-        if (err) return res.json({ success: false, error: err });
+        if (err) {
+            console.error("An error occured when saving the new account to the database");
+            return res.json({ success: false, error: err });
+        }
+        console.info("New account has been created and stored with success");
         return res.json({ success: true });
     });
 };
@@ -41,7 +54,11 @@ exports.putAccount = async function (req, res) {
 exports.deleteAccount = async function (req, res) {
     const { id } = req.body;
     Account.findByIdAndRemove(id, (err) => {
-        if (err) return res.send(err);
+        if (err){ 
+            console.error("The delete order was given, but was not executed by the database. This may be due to a connection error.");
+            return res.send(err);
+        }
+        console.info("The account has been deleted");
         return res.json({ success: true });
     });
 };
