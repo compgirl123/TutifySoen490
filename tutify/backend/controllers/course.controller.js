c6cb602200c2259dafe729acd6b0c61ac2cfa561
Pierre Watine <watinepierre@gmail.com>
2020-01-24 23:25:54 -0500

    Logging on course controller

--- tutify/backend/controllers/course.controller.js
+++ tutify/backend/controllers/course.controller.js
@@ -4,62 +4,79 @@ const Tutor = require('../models/models').Tutor;
 // this method fetches all available courses in our database
 exports.getCourses = async function (req, res) {
   Course.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
+    if (err) {
+      console.error("The courses were not found");
+      return res.json({ success: false, error: err });
+    }
+    console.info("The courses were found");
     return res.json({ success: true, data: data });
   });
 };
 
 // this method adds a tutor to an existing course
 exports.addTutorToCourse = async function (req, res) {
-  const { course_id, tutor} = req.body;
+  const { course_id, tutor } = req.body;
 
   Course.findByIdAndUpdate(course_id,
     { "$push": { "tutors": tutor } },
     { "new": true, "upsert": true },
     function (err, course) {
-      if (err) return res.json({ success: false, error: err });
+      if (err) {
+        console.error("The tutor couldn't get added to the course due to not finding the course");
+        return res.json({ success: false, error: err });
+      }
       let newCourse = {
         students: [],
         course: course._id,
-    }
+      }
       Tutor.findByIdAndUpdate(tutor,
         { "$push": { "courses": newCourse } },
         { "new": true, "upsert": true },
         function (err, tutor) {
-          if (err) throw err;
-  
-      return res.json({ success: true, course: course });
-      });
-  });
+          if (err) {
+            console.error("The tutor couldn't get added to the course due to not finding the tutor");
+            throw err;
+          }
+
+          console.info("The tutor was successfully added to the course");
+          return res.json({ success: true, course: course });
+        });
+    });
 };
 
 // this method adds a course to the database
 exports.addCourseToDb = async function (req, res) {
-  const { name, description, educationLevel, tutor} = req.body;
-  
+  const { name, description, educationLevel, tutor } = req.body;
+
   let course = new Course();
-  
+
   //create course
   course.name = name;
   course.description = description;
   course.educationLevel = educationLevel;
   course.tutors = tutor;
-  
+
   course.save(function (err, course) {
 
-    if (err) return res.json({ success: false, error: err });
+    if (err) {
+      console.error("The course couldn't get added to the database (API request failed)");
+      return res.json({ success: false, error: err });
+    }
     let newCourse = {
       students: [],
       course: course._id,
-  }
+    }
     Tutor.findByIdAndUpdate(tutor,
       { "$push": { "courses": newCourse } },
       { "new": true, "upsert": true },
       function (err, tutor) {
-        if (err) throw err;
-
-    return res.json({ success: true, course: course });
-    });
+        if (err) {
+          console.error("The course couldn't get added as the course's tutor was not found");
+          throw err;
+        }
+        console.info("The course was successfully added to the database");
+        return res.json({ success: true, course: course });
+      });
 
   });
 };
