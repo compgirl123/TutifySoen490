0a64b61ba89b783e4df913506b461b813128294d
compgirl123 <claudia.f.feochari@hotmail.com>
2020-02-05 22:59:19 -0500

    #168 #173 : Added routing fixes

--- tutify/src/App.js
+++ tutify/src/App.js
@@ -28,32 +28,6 @@ import NotFoundPage from './components/NotFoundPage';
 function MainPage() {
   return (
     <Router>
-      <Route exact path="/" component={Main} />
-      <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/search" component={SearchTutors} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/profile" component={ProfilePage} />
-      <Route exact path="/students" component={StudentList} />
-      <Route exact path="/students/:file" component={StudentList} />
-      <Route exact path="/tutors" component={TutorList} />
-      <Route exact path="/tutors/:file" component={TutorList} />
-      <Route exact path= "/courseList/:id" component= {CourseList}/>
-      <Route exact path= "/tutorCourses/:file" component = {MyCourses}/>
-      <Route exact path= "/courses" component = {MyCourses}/>
-      <Route exact path="/dashboard" component={UserDashboard} />
-      <Route exact path= "/uploadingDocs" component = {UploadDocuments}/>
-      <Route exact path= "/postblog" component={BlogPost} />
-      <Route exact path= "/doclist" component={DocList} />
-      <Route exact path= "/viewCourse/:coursename" component={ViewCourse} />
-      <Route exact path= "/document/:filename" component={Document} />
-      <Route exact path= "/announcements" component={Announcements} />
-      <Route exact path= "/tutdoc" component={Tutordocs} />
-      <Route exact path= "/doc" component={Studentdocs} />
-      <Route exact path= "/doc/:studentid" component={Studentdocs} />
-      <Route exact path= "/resources" component={ResourceLevels} />
-      <Route exact path= "/primary" component={ResourcePage} />
-      <Route exact path= "/secondary" component={ResourcePage} />
-      <Route exact path= "/postsecondary" component={ResourcePage} />
       <Switch>
         <MainRoute exact path= "/" component={Main} />
         <Route exact path= "/signup" component={SignUp} />
@@ -61,7 +35,11 @@ function MainPage() {
         <PrivateRoute  exact path= "/search" component={SearchTutors} />
         <PrivateRoute  exact path= "/profile" component={ProfilePage} />
         <PrivateRoute  exact path= "/students" component={StudentList} />
-        <PrivateRoute  exact path="/students/:file" component={StudentList} />
+        <PrivateRoute  exact path= "/tutors" component={TutorList} />
+        <PrivateRoute  exact path= "/tutors/:file" component={TutorList} />
+        <PrivateRoute  exact path= "/tutdoc" component={Tutordocs} />
+        <PrivateRoute  exact path= "/tutorCourses/:file" component = {MyCourses}/>
+        <PrivateRoute  exact path= "/students/:file" component={StudentList} />
         <PrivateRoute  exact path= "/courseList/:id" component= {CourseList}/>
         <PrivateRoute  exact path= "/tutorCourses/:file" component = {MyCourses}/>
         <PrivateRoute  exact path= "/courses" component = {MyCourses}/>
