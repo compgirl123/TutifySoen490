d1c3fae775d6f9170227f6d4040a5c5aad6e6722
compgirl123 <claudia.f.feochari@hotmail.com>
2020-02-09 16:27:33 -0500

    #167: Fixes for upload docs

--- tutify/src/components/Documents/UploadDocuments.js
+++ tutify/src/components/Documents/UploadDocuments.js
@@ -91,7 +91,7 @@ export class UploadDocuments extends Component {
     event.preventDefault();
     const formData = new FormData();
     formData.append('file', this.state.file);
-    formData.append('adminTutor', this.state.user_id);
+    formData.append('admin', this.state.user_id);
     formData.append('name', this.state.file.name);
     await axios.post("/uploadFile", formData).then(res => {
     }).catch(err => {
