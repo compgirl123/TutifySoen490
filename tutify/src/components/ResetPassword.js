e06a3647e525f881ea11df6c9ea68914b428bc0a
tanyamultani <tanyamultani1@gmail.com>
2020-02-23 14:45:05 -0500

    fixed backend functions + added consoles  #180

--- tutify/src/components/ResetPassword.js
+++ tutify/src/components/ResetPassword.js
@@ -4,7 +4,6 @@ import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import large_tutify from './../assets/large_tutify.png';
 import * as tutifyStyle from '../styles/SignUp-styles';
 import { withStyles } from "@material-ui/core/styles";
 import NavBar from './NavBar';
@@ -21,7 +20,6 @@ export class ForgotPassword extends React.Component {
       data: [],
       newPassword: "",
       confirmPassword: "",
-      token:"",
     };
     this.resetPassword = this.resetPassword.bind(this);
   }
@@ -30,28 +28,45 @@ export class ForgotPassword extends React.Component {
   resetPassword = () => {
 
     var url = window.location.href.split("/");
-    this.setState({token :url[url.length-1] });
+    var token = url[url.length - 1];
 
-    if(this.state.newPassword === this.state.confirmPassword){
-        axios.post('/api/resetPassword', {
-            password: this.state.newPassword,
-            resetPasswordToken: this.state.token
-        })
+    if (this.state.newPassword === this.state.confirmPassword) {
+      console.info("Updating password in db");
+      axios.post('/api/resetPassword', {
+        password: this.state.newPassword,
+        resetPasswordToken: token
+      })
         .then((res) => {
-        
-          if(res.data.success){
-            swal("Password successfully reset!", "", "success");
+
+          if (res.data.success) {
+            swal("Password successfully reset!", "", "success").then((res) => {
+              window.location = "/login";
+
+            }, (error) => {
+              console.error("Could not display swal alert. " + error);
+            });
           }
-          else{
-            swal("Unable to reset password!", "The reset password token has expired or is invalid.", "error");
+          else {
+            swal("Unable to reset password!", "The reset password token has expired or is invalid.", "error").then((res) => {
+              window.location = window.location.href;
+
+            }, (error) => {
+              console.error("Could not display swal alert. " + error);
+            });
           }
-           
+
         }, (error) => {
-            console.error(error);
+          console.error("Could not reset password (API call error) " + error);
         });
     }
-    else{
-        swal("Invalid password!", "New password and confirm password do not match.", "error");
+    else {
+
+      swal("Invalid password!", "New password and confirm password do not match.", "error").then((res) => {
+        window.location = window.location.href;
+
+      }, (error) => {
+        console.error("Could not display swal alert. " + error);
+      });
     }
   }
 
@@ -64,62 +79,69 @@ export class ForgotPassword extends React.Component {
         <Container component="main">
           <CssBaseline />
           <div className="paper">
-            
-                
-              <Grid container spacing={3}
-                direction="column"
-                style={{ minHeight: '55vh' }}
-                justify="center"
-                alignItems="center">
 
-                <Grid item xs={12}>
 
-                <Typography variant="h6" color="inherit" style={{fontWeight:"bold"}} >
-                Reset Password
+            <Grid container spacing={3}
+              direction="column"
+              style={{ minHeight: '55vh' }}
+              justify="center"
+              alignItems="center">
+
+              <Grid item xs={12}>
+
+                <Typography variant="h6" color="inherit" style={{ fontWeight: "bold" }} >
+                  Reset Password
                 </Typography>
-                </Grid>
-
-                <Grid item xs={12}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    style={{ width: 350 }}
-                    id="email"
-                    label="New Password"
-                    name="email"
-                    margin="dense"
-                    onChange={e => this.setState({ newPassword: e.target.value })}
-                    
-                  />
-                </Grid>
-
-                <Grid item xs={12}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    style={{ width: 350 }}
-                    label="Confirm Password"
-                    margin="dense"
-                    onChange={e => this.setState({ confirmPassword: e.target.value })}
-                    
-                  />
-                </Grid>
-                
-
-                <Grid item xs={12} >
-                  <Button
-                    name="submit"
-                    type="submit"
-                    style={{ width: 350 }}
-                    variant="contained"
-                    className="loginSubmit"
-                    onClick={() => this.resetPassword()}
-                  >
-                    Update Password
-                  </Button>
-                
-                </Grid>
               </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  style={{ width: 350 }}
+                  label="New Password"
+                  margin="dense"
+                  type="password"
+                  id="newPassword"
+                  onChange={e => this.setState({ newPassword: e.target.value })}
+
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  style={{ width: 350 }}
+                  label="Confirm Password"
+                  margin="dense"
+                  id="confirmPassword"
+                  type="password"
+                  onChange={e => this.setState({ confirmPassword: e.target.value })}
+
+                />
+              </Grid>
+
+
+              <Grid item xs={12} >
+                <Button
+                  name="submit"
+                  type="submit"
+                  style={{ width: 350 }}
+                  variant="contained"
+                  className="loginSubmit"
+                  onClick={() => this.resetPassword()}
+                  InputProps={{
+                    classes: {
+                      notchedOutline: classes.notchedOutline
+                    }
+                  }}
+                >
+                  Update Password
+                  </Button>
+
+              </Grid>
+            </Grid>
           </div>
           <Footer />
         </Container>
