2d3500c350a7384a6ec7c961d8de8bc480793297
Pierre Watine <watinepierre@gmail.com>
2020-03-08 18:05:30 -0400

    #192: Added function to add question to db

--- tutify/backend/controllers/quiz.controller.js
+++ tutify/backend/controllers/quiz.controller.js
@@ -1,4 +1,5 @@
 const Quizes = require('../models/models').Quizes;
+const Questions = require('../models/models').Questions;
 
 // this method fetches all available quizes in our database
 exports.getQuizes = async function (req, res) {
@@ -9,13 +10,13 @@ exports.getQuizes = async function (req, res) {
     else if (req.session.userInfo.__t == "student") {
         id.push(req.query.tutor);
     }
-    Quizes.find({ tutorId: { $in: id } }, function (err, video) {
+    Quizes.find({ tutorId: { $in: id } }, function (err, quiz) {
         if (err) {
             console.error("The quizes were not found");
             return res.json({ success: false, error: err })
         }
         console.info("The quizes were found");
-        return res.json({ success: true, data: video });
+        return res.json({ success: true, data: quiz });
     });
 };
 
@@ -28,27 +29,25 @@ exports.getSelectQuizes = async (req, res) => {
     else if (req.session.userInfo.__t == "student") {
         id.push(req.query.tutor);
     }
-    await Quizes.find({ tutorId: { $in: id }, course: req.query.tutorClasses[req.query.courseSelected] }, async (err, video) => {
+    await Quizes.find({ tutorId: { $in: id }, course: req.query.tutorClasses[req.query.courseSelected] }, async (err, quiz) => {
         if (err) {
             console.error("The quizes were not found");
             return await res.json({ success: false, error: err })
         }
         console.info("The quizes were found");
-        return await res.json({ success: true, data: video });
+        return await res.json({ success: true, data: quiz });
     });
 
 };
 
-// this method adds a new video to the database
-exports.addVideo = async function (req, res) {
-    const { title, description, questions, choices, answers, tutorId, course } = req.body;
-    // new video to be added by tutor
+// this method adds a new quiz to the database
+exports.addQuiz = async function (req, res) {
+    const { title, description, questions, tutorId, course } = req.body;
+    // new quiz to be added by tutor
     let quizes = new Quizes();
     quizes.title = title;
     quizes.description = description;
     quizes.questions = questions;
-    quizes.choices = choices;
-    quizes.answers = answers;
     quizes.tutorId = tutorId;
     quizes.course = course;
     quizes.save(function (err, quizes) {
@@ -62,8 +61,30 @@ exports.addVideo = async function (req, res) {
     });
 };
 
+
+// this method adds a new question to the database
+exports.addQuestion = async function (req, res) {
+    const { question, choices, answerIndex, tutorId, course } = req.body;
+    // new quiz to be added by tutor
+    let questions = new Questions();
+    questions.question = question;
+    questions.choices = choices;
+    questions.answerIndex = answerIndex;
+    questions.tutorId = tutorId;
+    questions.course = course;
+    questions.save(function (err, question) {
+        if (err) {
+            console.error(err);
+            console.error("The question couldn't get added to the database (API request failed)");
+            return res.json({ success: false, error: err });
+        }
+        console.info("The question was successfully added to the database");
+        return res.json({ success: true, data: question });
+    });
+};
+
 // this method deletes a new quiz to the db
-exports.deleteVideo = async function (req, res) {
+exports.deleteQuiz = async function (req, res) {
     const { _id } = req.body;
     // selected quiz to delete by tutor
     Quizes.findByIdAndRemove(_id, (err) => {
