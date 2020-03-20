5428c7ddc5e6913e7e771c63ed90541bb3606b6f
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2020-03-20 15:32:19 -0400

    #206 removed unused lines in server.js that will be used for frontend deployment

--- tutify/server.js
+++ tutify/server.js
@@ -1,60 +1,13 @@
-const mongoose = require('mongoose');
 const express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const router = require('./backend/routes');
 var session = require('express-session');
-var multer = require('multer');
-const GridFsStorage = require("multer-gridfs-storage");
-const crypto = require('crypto');
 const API_PORT = 8080;
 const app = express();
 const path = require("path");
 app.use(cors({credentials: true, origin: true}));
-var uploadController = require('./backend/controllers/uploaded_files.controller')
-
-// this is our MongoDB database
-const dbRoute =
-  'mongodb+srv://app:Mdfi8g6IMFagCOOE@tutify-q6b06.mongodb.net/tutify?retryWrites=true&w=majority';
-
-// connects our back end code with the database
-mongoose.connect(dbRoute, { useNewUrlParser: true, useUnifiedTopology: true });
-// connection
-let db = mongoose.connection;
-let gfs;
-const conn = db.once('open', () => {
-   // init stream
-   gfs = new mongoose.mongo.GridFSBucket(conn.db, {
-    bucketName: "uploads"
-  });
-  console.log('connected to the database');
-});
- // Storage
- const storage = new GridFsStorage({
-  url: dbRoute,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString("hex") + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: "uploads"
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
-const upload = multer({
-  storage
-});
-
-// checks if connection with the database is successful
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // only made for logging and bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -67,9 +20,6 @@ app.use(session({secret:"sdshkgjdhgkhgkjsd322k3j4nkjkjhb3", resave:false, saveUn
 // append /api for our http requests
 app.use('/api', router);
 app.use('/public', express.static('public'));
-
-// uploading files routes
-app.post('/uploadFile', upload.single('file'), uploadController.addUploadedFiles);
 
 // file upload requirements
 app.use(express.json());
