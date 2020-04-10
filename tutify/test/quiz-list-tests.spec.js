74468bba03c17a2bf4f3bd62bc2caa84826d3304
Pierre Watine <watinepierre@gmail.com>
2020-04-10 02:08:41 -0400

    #223: Finished QuizList test

--- tutify/test/quiz-list-tests.spec.js
+++ tutify/test/quiz-list-tests.spec.js
@@ -5,19 +5,20 @@ import ChooseCourseAndQuiz, { ChooseCourseAndQuiz as ChooseCourseAndQuizClass } 
 import { createMount } from '@material-ui/core/test-utils';
 import { configure } from 'enzyme';
 import Adapter from "enzyme-adapter-react-16";
+import { Typography } from "@material-ui/core";
 
 
 var json = require("./testDb/quiz-list.json");
 
 configure({ adapter: new Adapter() });
-describe('The quiz page ', () => {
+describe('The quiz list page ', () => {
     let mount;
 
     beforeAll(() => {
         mount = createMount();
     });
 
-    it('should display the questions properly', () => {
+    it('should display quizzes properly', () => {
         // The value sent in the input
         const mockedEvent = {
             params: { id: '5e2a278df179bdc10d28385e' }
@@ -37,49 +38,28 @@ describe('The quiz page ', () => {
         // All the mounting and state setting
         const wrapper = mount(<ChooseCourseAndQuiz classes={mockedClass} match={mockedEvent}></ChooseCourseAndQuiz>);
         const shallowwrapper = wrapper.find(ChooseCourseAndQuizClass);
-        shallowwrapper.setState({ quizzes: json.data, open:true, newValue:"NEWVALUE", categoryOptions:mockedOptions});
+        shallowwrapper.setState({
+            quizzes: json.data,
+            open:true,
+            tutorFirstName: "Ronald",
+          tutorLastName:"McDonald", 
+            newValue:"NEWVALUE", 
+            categoryOptions:mockedOptions,
+            courseIndex:0,
+            description:json.data.description
+        });
 
+        // Displays the quiz information properly
+        expect(wrapper.find(Typography).at(0).text()).toBe("Tutify");
+        expect(wrapper.find(Typography).at(1).text()).toBe("Tutify");
+        expect(wrapper.find(Typography).at(2).text()).toBe("");
+        expect(wrapper.find(Typography).at(3).text()).toBe("Ronald McDonald's Quizzes");
+        expect(wrapper.find(Typography).at(4).text()).toBe("Test");
+        expect(wrapper.find(Typography).at(5).text()).toBe("Company");
+        expect(wrapper.find(Typography).at(6).text()).toBe("Team");
 
-        // // Finding the Button component 
-        // const button_component = wrapper.find(Button).at(0);
-        // // Check if the "add quiz" button exists
-        // expect(button_component.exists()).toBeTruthy();
-
-        // // Displays the question properly
-        // expect(wrapper.find('.P').text()).toBe("What hotel does Edward Calederon Work at");
-
-        // // Displays the answers properly
-        // expect(wrapper.find('.ANSWERSUL').text()).toBe("A Majestic MirageB Majestic EleganceC Majestic Elegance ClubD Majestic Colonial");
+        
 
     });
-
-    it('should pop-up a dialog box when the state boolean changes', () => {
-        // The value sent in the input
-        const mockedEvent = {
-            params: { id: '5e2a278df179bdc10d28385e' }
-        }
-        const mockedOptions = ["COURSE1", "COURSE2", "COURSE3"]
-        const mockedClass = {
-            appBarSpacer: "APPBARSPACER",
-            container: "CONTAINER",
-            card: "CARD",
-            media: "MEDIA",
-            deleteIconButton: "DELETEICONBUTTON",
-            deleteCourseButton: "DELETECOURSEBUTTON",
-            formControl:"FORMCONTROL",
-            addVideoButton:"ADDVIDEOBUTTON"
-        }
-
-
-        // All the mounting and state setting
-        const wrapper = mount(<ChooseCourseAndQuiz classes={mockedClass} match={mockedEvent}></ChooseCourseAndQuiz>);
-        const shallowwrapper = wrapper.find(ChooseCourseAndQuizClass);
-        shallowwrapper.setState({ quizzes: json.data, open:true, newValue:"NEWVALUE", categoryOptions:mockedOptions});
-
-
-    
-    });
-
-
 
 }); 
